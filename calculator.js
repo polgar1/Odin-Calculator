26742593e8b65daf7ea4146c84008bb6ad6a7a28
Punga911 <punga9111@gmail.com>
2024-09-14 18:28:09 +0300

    Basic functionality

---
+++ calculator.js
@@ -0,0 +1,85 @@
+const result = document.querySelector("#result");
+result.textContent = "Result: " + 0;
+let x = 0;
+let y = 0;
+let op = "";
+const buttons = document.querySelectorAll("button");
+
+function Add(num1, num2)
+{
+    return num1 + num2;
+}
+
+function Subtract(num1, num2)
+{
+    return num1 - num2;
+}
+
+function Multiply(num1, num2)
+{
+    return num1 * num2;
+}
+
+function Divide(num1, num2)
+{
+    return num1 / num2;
+}
+
+function Operate(num1, num2, operator)
+{
+    switch (operator)
+    {
+        case "+":
+            return Add(num1, num2);
+            break;
+        case "-":
+            return Subtract(num1, num2);
+            break;
+        case "x":
+            return Multiply(num1, num2);
+            break;
+        case "/":
+            return Divide(num1, num2);
+            break;
+    }
+}
+
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => { 
+        if (x == 0 && button.className == "NUM")
+        {
+            x = Number(button.id);
+        }
+        else if (x != 0 && op == "" && button.className == "NUM")
+        {
+            x = x * 10 + Number(button.id);
+        }
+        else if(x != 0 && op == "" && button.className == "OP")
+        {
+            op = button.textContent;
+        }
+        else if (op != "" && button.className == "NUM" && y == 0)
+        {
+            y = Number(button.id);
+        }
+        else if (button.className == "NUM" && y != 0)
+        {
+            y = y * 10 + Number(button.id);
+        }
+        
+        result.textContent = "Result: " + x + op;
+        if (y)
+        {
+            result.textContent += y;
+        }
+        if(y != 0 && button.id == "equal")
+        {
+            x = Operate(x, y, op);
+            op = "";
+            y = 0;
+            result.textContent ="Result: " + x;
+
+        }
+    })
+});
